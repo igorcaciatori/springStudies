fdd836a396a8e8f9e1c94dd0d9152aba50dcab33
Igor Caciatori <147560622+igorcaciatori@users.noreply.github.com>
2024-11-28 21:12:07 -0300

    Instalação pelo Spring Initializr

---
+++ src/main/java/com/udemystudies/springStudies/SpringStudiesApplication.java
@@ -0,0 +1,12 @@
+package com.udemystudies.springStudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudiesApplication.class, args);
+	}
+}
